45bf81ce895a21a18eaa8f182dc36b62079ac7d6
Vadzim Mikanovich <liontv05@gmail.com>
2024-01-02 22:12:39 +0100

    Initial Commit

---
+++ e-car_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  e-car_app
+//
+//  Created by Wadim Mikanowicz on 02/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ e-car_app/e_car_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_car_appApp.swift
+//  e-car_app
+//
+//  Created by Wadim Mikanowicz on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct e_car_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
